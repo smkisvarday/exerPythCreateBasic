c5b221761dab33d6d8a1fbb1179f9ec491fcabb3
smkisvarday <susannahkisvarday@hotmail.com>
2021-12-15 19:53:24 -0500

    Completed1

---
+++ calc.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "2eb79306",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Type the first number: 2\n",
+      "Type the second number: 3\n",
+      "The sum is:  5\n"
+     ]
+    }
+   ],
+   "source": [
+    "first_number = int(input('Type the first number: ')) ;\\\n",
+    "second_number = int(input('Type the second number: ')) ;\\\n",
+    "print(\"The sum is: \", first_number + second_number)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "342a6625",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
